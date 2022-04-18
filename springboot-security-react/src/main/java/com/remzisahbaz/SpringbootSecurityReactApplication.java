702a0bd8e24222fcb44091573ba89f68fdcc6b0f
RShbz <remzisahbaz@outlook.com>
2022-04-18 15:13:10 +0300

    create new spring boot project

---
+++ springboot-security-react/src/main/java/com/remzisahbaz/SpringbootSecurityReactApplication.java
@@ -0,0 +1,13 @@
+package com.remzisahbaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityReactApplication.class, args);
+	}
+
+}
